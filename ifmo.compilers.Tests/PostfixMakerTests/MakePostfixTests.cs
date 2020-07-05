691d489213336e8b8e86b1699733c2c6b28cb07d
Anton Orekhovskiy <anton.o.0619@gmail.com>
2020-07-05 19:26:11 +0600

    ifmo.compilers.Tests added

---
+++ ifmo.compilers.Tests/PostfixMakerTests/MakePostfixTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ifmo.compilers.Tests.PostfixMakerTests
+{
+    [TestFixture]
+    class MakePostfixTests
+    {
+        [Test]
+        public void MakePostfixTest1()
+        {
+
+        }
+    }
+}
